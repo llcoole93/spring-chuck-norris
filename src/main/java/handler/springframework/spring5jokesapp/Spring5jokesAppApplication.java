58d200e1d08cfe7a1900b7a01d50c41173f49a7f
Mortimus <llcoole93@gmail.com>
2021-04-01 17:49:29 -0400

    code

---
+++ src/main/java/handler/springframework/spring5jokesapp/Spring5jokesAppApplication.java
@@ -0,0 +1,13 @@
+package handler.springframework.spring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5jokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5jokesAppApplication.class, args);
+	}
+
+}
